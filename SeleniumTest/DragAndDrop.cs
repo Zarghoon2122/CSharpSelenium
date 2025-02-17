1740bfda7f2fd85fc3240c58794a82655a78ea84
Zarghoon2122 <zarghon.danish@gmail.com>
2025-02-17 13:09:51 -0500

    Tests added

---
+++ SeleniumTest/DragAndDrop.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumTest
+{
+    class DragAndDrop
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Url = "https://demoqa.com/droppable";
+        }
+
+        [Test]
+
+        public void DragandDrop()
+        {
+            Actions a = new Actions(driver);
+            a.DragAndDrop(driver.FindElement(By.Id("draggable")), driver.FindElement(By.Id("droppable"))).Perform();
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            if (driver != null)
+            {
+                //driver.Quit();    // Close the browser
+                driver.Dispose(); // Explicitly dispose of WebDriver
+                driver = null;    // Clear the reference
+            }
+        }
+
+    }
+}
---
+++ SeleniumTest/MoveElement.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumTest
+{
+    public class MoveElement
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Url = "https://rahulshettyacademy.com/#/index";
+        }
+
+        [Test]
+
+        public void MoveElementTest()
+        {
+            Actions a = new Actions(driver);
+            a.MoveToElement(driver.FindElement(By.CssSelector("a.dropdown-toggle"))).Perform();
+            //driver.FindElement(By.XPath("//ul[@class='dropdown-menu']/li[1]/a")).Click();
+
+            //Click element using 'MoveToElement' action class
+            a.MoveToElement(driver.FindElement(By.XPath("//ul[@class='dropdown-menu']/li[1]/a"))).Click().Perform();
+            
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            if (driver != null)
+            {
+                //driver.Quit();    // Close the browser
+                driver.Dispose(); // Explicitly dispose of WebDriver
+                driver = null;    // Clear the reference
+            }
+        }
+    }
+}
